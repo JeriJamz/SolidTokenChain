8777b5a04b0d92e2b81e62351b34bae0cf2fbc9a
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-08-21 10:16:47 -0500

    Update elypticCurve.py

--- elypticCurve.py
+++ elypticCurve.py
@@ -14,13 +14,25 @@ class point:#ok ok the point class make the 2 ends points and two curve points p
         self.b = b
         self.x = x
         self.y = y
-        if self.y**2 != x**3 + a*x + b:
+        if self.x is None and self.y is None:#Putting the points at None is why its set to infinity
+            return#where my memes... dont mind this comment
+        if self.y**2 != x**3 + a*x + b:#this statememnt fails I think due to preset range limits
             raise ValueError('{},{} dont fit the scheme'.format(x,y))
 
     def eq(self,other):#other... python is the real abstraction
         return self.x == other.x and self.y/#so I can remember the /
         and self.a and self.b
 
+    def add(self, other):#this so you can get other points involves but I gotta work on the UserNput so...
+        if self.a != other.a or self.b != other.b:
+            raise ValueError(f'Points {},{} are not the same curve'.format(self,other))
+
+    if self.x is None:
+        return other.x
+
+    if self.y is None:
+        return other.y
+        
     def usN(self,open, openTwo):#this my personal class I think this take user input
         TimePut(f'Enter two Diffrent point',/
                ,'Point One <Enter>')
