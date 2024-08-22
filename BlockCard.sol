c793adbcdb1d3052358ca82f66663831a59229e4
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-08-22 16:30:50 -0500

    Create BlockCard.sol

---
+++ BlockCard.sol
@@ -0,0 +1,59 @@
+//I have no idea what this is so now its a banking card with a reward system
+pragma solidity >=0.7.0<0.9.0//best style header idc
+
+contract Exchange{
+
+    uint Tag;//this keep me wit variable..."word" no str
+    uint cardNumber;
+    
+    address public minter;
+    mapping(address => unit) public balance
+
+    constructor(){
+
+        minter = msg.sender;
+
+    }
+
+    fuction mint(address receiver, uint amount public){
+
+        require(msg.sender == sender);
+        balances[receiver] +=amount;
+    
+}
+
+    
+
+    function set(uint Tokes) public{
+
+        Tag = Tokes;
+
+    event Sent(address from, address to, uint amount)
+    senTik = Sent
+    if(senTik.lenth++){
+
+        CoupenThing();
+
+        }
+
+    }
+
+    function CoupenThing(uint swip){
+
+    for(uint = 0; i < swip.length;i++){
+
+        "for a free slushie you need",balance.length,"/10 swipes";
+
+        }
+
+    Swipebalnce = swip.length;
+
+    }
+
+    function get() public view returns(uint){//this a cold syntax
+
+        return swip.length; //this should work
+
+    }
+
+}
